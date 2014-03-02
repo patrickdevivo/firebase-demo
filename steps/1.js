46b2c6797b0c6146d88b0717c7a6bec92b838e53
Patrick DeVivo <patrick.devivo@gmail.com>
2014-03-01 22:41:43 -0500

    fixes

--- steps/1.js
+++ steps/1.js
@@ -1,3 +1,18 @@
 var firebaseRef = new Firebase('https://fb-amherst.firebaseio.com/'),
     chatRoom = firebaseRef.child('chatroom')
+    uid = Math.random().toString(36).substring(2)
     ;
+
+$(function() {
+    $('#send').on('click', function (e) {
+        e.preventDefault();
+        var message = $('#message').val();
+        var now = new Date();
+        chatRoom.push({
+            message: message,
+            date: now.toString(),
+            user: uid
+        });
+        $('#message').val('');
+    });
+})
